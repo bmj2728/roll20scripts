9d47afb5623165400d469133746954d7e15d6c25
Brian Jipson <bmj2728@gmail.com>
2026-08-22 21:42:30 -0400

    add passiveWisdom script and update PartyMan to include styled buttons

--- partyman.js
+++ partyman.js
@@ -3,10 +3,22 @@ const getParty = () => {
 }
 
 on('ready', () => {
-    log("starting PartyMan")
-    getParty().forEach(c => {
-        sendChat('PartyMan', `Party Member: ${c.get("name")}`)
-    })
+    log("Starting PartyMan")
+
+    // Define button styling inside the HTML string
+
+    let command = "!pm party"
+    let commandName = "Display Party"
+
+    let style = "background-color: #7e22ce; color: white; padding: 5px 10px; border-radius: 4px; text-decoration: none; font-weight: bold;";
+
+    let htmlButton = `<a style="${style}" href="${command}">${commandName}</a>`;
+
+    sendChat('PartyMan', "PartyMan HTML:\n" + htmlButton)
+
+    let markdownButton = `[${commandName}](${command})`
+
+    sendChat('PartyMan', "PartyMan MD:\n" + markdownButton)
 
     on('chat:message', async msg => {
         if (msg.type !== 'api' || !/^!pm\b/i.test(msg.content)) return;
